5952c42eac7852e26a339a5ad52ab1fdbd611a99
Matt Hughes <mattphihughes@gmail.com>
2014-02-16 22:18:02 -0500

    Added presenter class

--- src/com/nickelheim/activities/WelcomeActivity.java
+++ src/com/nickelheim/activities/WelcomeActivity.java
@@ -3,14 +3,27 @@ package com.nickelheim.activities;
 import com.nickelheim.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends Activity
 {
+    
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+    }
+
+
+    public void startLogin(View view) {
+	
+    }
+
+
+    public void startRegister(View view) {
+
     }
 }
---
+++ src/com/nickelheim/presenters/WelcomeButtonListener.java
@@ -0,0 +1,17 @@
+package com.nickelheim.presenters;
+
+
+
+public class WelcomeButtonListener {
+
+
+    public void login() {
+
+    }
+
+    public void register() {
+
+    }
+
+
+}
