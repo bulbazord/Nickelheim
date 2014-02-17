12873a2cde07ba8579d7671eb6fbd44e1c01f496
Alexander Langford <nirvashtzero@gmail.com>
2014-02-16 23:39:36 -0500

    Fixed some stupid stuff

--- src/com/nickelheim/views/AbstractWelcomeActivity.java
+++ src/com/nickelheim/views/AbstractWelcomeActivity.java
@@ -3,6 +3,6 @@ package com.nickelheim.views;
 import android.view.View;
 import android.app.Activity;
 public abstract class AbstractWelcomeActivity extends Activity {
-    public void startLogin(View view);
-    public void startRegister(View view);
+    public abstract void startLogin(View view);
+    public abstract void startRegister(View view);
 }
