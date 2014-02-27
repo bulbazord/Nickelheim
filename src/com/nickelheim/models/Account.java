723dce83b36f6db59d3c1404588aed5431d17e7a
Matt Hughes <mattphihughes@gmail.com>
2014-02-27 14:17:53 -0500

    Prettified Account Javadocs

--- src/com/nickelheim/models/Account.java
+++ src/com/nickelheim/models/Account.java
@@ -2,8 +2,12 @@ package com.nickelheim.models;
 
 /**
  * Represents the account that a user creates after successful
- * login/registration
- * 
+ * login/registration.
+ *
+ * This is an account in the domain sense of the word; we are not talking about
+ * the account the user has with our service. This is a financial account, not
+ * application-specific.
+ *
  * @author aphivantrakul
  */
 public class Account {
@@ -12,37 +16,67 @@ public class Account {
     private String lastName;
     private String email;
     
-    //Constructor
+    /**
+     * Constructs an Account object, given identifying information about the
+     * corresponding user.
+     *
+     * @param username
+     * @param firstName
+     * @param lastName
+     * @param email
+     */
     public Account(String username, String firstName, String lastName, String email) {
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
-    
-    //toString method that is not currently being used
-    //prints out all the instance variables in a pretty way
+
+    /**
+     * Returns a String representation of this Account.
+     *
+     * Presently not being used.
+     *
+     * @return String a representation of this object
+     */
+    @Override
     public String toString() {
         return "username: " + username + " first name: " + firstName 
                 + " last name: " + lastName + " email: " + email;
     }
     
-    //getter for username instance variable
+    /**
+     * Returns the username of the user who owns this account.
+     *
+     * @return String the username
+     */
     public String getUsername() {
         return username;
     }
     
-    //getter for firstname instance variable
+    /**
+     * Returns the first name of the user who owns this account.
+     *
+     * @return String the user's name
+     */
     public String getFirstName() {
         return firstName;
     }
     
-    //getter for lastname instance variable
+    /**
+     * Returns the last name of the user who owns this account.
+     *
+     * @return String the last name
+     */
     public String getLastName() {
         return lastName;
     }
     
-    //getter for email instance variable
+    /**
+     * Returns the email of the user who owns this account.
+     *
+     * @return String the email
+     */
     public String getEmail() {
         return email;
     }
